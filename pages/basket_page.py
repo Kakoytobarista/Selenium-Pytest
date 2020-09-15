960999284ce3e3c0835c4edb74ab1854ba51aad1
Aslan <baristan1@rambler.ru>
2020-09-15 21:23:23 +0300

    add file for optimization new test cases

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -12,3 +12,10 @@ class BasketPage(BasePage):
     def is_not_element_in_basket(self):
         assert self.is_not_element_present(*BasePageLocators.SELECTOR_NOT_ITEM_IN_BASKET), \
             "select item in basket"
+
+    def should_see_product_in_basket(self):
+        self.link_to_basket()
+        login_page = BasketPage(self.browser, self.browser.current_url)
+        login_page.should_product_in_basket()
+
+
