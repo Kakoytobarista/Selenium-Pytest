ab907d9bbb2476437515bf3504ac5cdc04194130
Aslan <baristan1@rambler.ru>
2020-09-16 00:38:47 +0300

    added markers for selecting my tests

--- test_main_page.py
+++ test_main_page.py
@@ -1,36 +1,39 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages. base_page import BasePage
+from pages.basket_page import BasketPage
 import pytest
 
 
-# def test_guest_can_go_to_login_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser, browser.current_url)
-#     login_page.should_be_login_page()
-
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
-    def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.go_to_login_page()
-        login_page = MainPage(page.browser, page.browser.current_url)
-        login_page.should_be_login_link()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
 
-def test_guest_should_see_registration_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+def test_guest_should_see_login_link(self, browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_registration_form()
+    page.go_to_login_page()
+    login_page = MainPage(page.browser, page.browser.current_url)
+    login_page.should_be_login_link()
 
 
-def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_form()
+# def test_guest_should_see_registration_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_registration_form()
+#
+#
+# def test_guest_should_see_login_form(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_form()
