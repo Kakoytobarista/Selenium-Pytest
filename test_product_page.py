e251cbf3b6c13faaccac575c3c11e569a39faf68
Aslan <baristan1@rambler.ru>
2020-09-16 23:58:55 +0300

    added fixture setup, added 2 tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,9 @@
 from selenium.common.exceptions import NoAlertPresentException
 from pages.product_page import ProductPage
 from pages .basket_page import BasketPage
+from pages .login_page import LoginPage
 import pytest
+import time
 
 
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -43,24 +45,24 @@ import pytest
 #     page.should_element_is_disappeared()
 #
 #
-@pytest.mark.need_review
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-
-@pytest.mark.need_review
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = ProductPage(browser, browser.current_url)
-    login_page.should_be_login_link()
-
-
+# @pytest.mark.need_review
+# def test_guest_should_see_login_link_on_product_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+#
+#
+# @pytest.mark.need_review
+# def test_guest_can_go_to_login_page_from_product_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+#     login_page = ProductPage(browser, browser.current_url)
+#     login_page.should_be_login_link()
+#
+#
 # @pytest.mark.need_review
 # def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
 #     link = "http://selenium1py.pythonanywhere.com/en-gb/"
@@ -82,6 +84,33 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 #     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 #     page = BasketPage(browser, link)
 #     page.open()
+#     time.sleep(3)
 #     page.should_add_product_in_basket()
+#     time.sleep(3)
 #     page.should_see_product_in_basket()
-#
+#     time.sleep(3)
+
+
+class TestUserAddBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/."
+        self.page = LoginPage(browser, link)
+        self.page.open()
+        self.page.register_new_user(email=any, password=any)
+        self.page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        print('gotcha')
+        page.should_not_be_success_message()
+        print('gotcha')
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.check_name_product_in_string_basket()
