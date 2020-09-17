06fd84a77d895ef3dcfcaef81c06ba02396c7a2c
Aslan <baristan1@rambler.ru>
2020-09-17 16:32:58 +0300

    deleted 2 not used imports

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,16 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
-from pages. base_page import BasePage
-from pages.basket_page import BasketPage
 import pytest
-import time
 
 
 @pytest.mark.login_guest
-class TestLoginFromMainPage():
+class TestLoginFromMainPage(object):
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
-        time.sleep(3)
         page.go_to_login_page()
-        time.sleep(3)
         login_page = LoginPage(browser, browser.current_url)
-        time.sleep(3)
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
@@ -37,6 +31,6 @@ def test_guest_should_see_registration_form(browser):
 
 def test_guest_should_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = MainPage(browser, link)
+    page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
