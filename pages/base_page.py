328d868d06c51b13fd44b9e1cb33e4a58dc701ae
Aslan <baristan1@rambler.ru>
2020-09-17 16:33:59 +0300

    switch bakset to basket

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
 import math
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,7 +20,7 @@ class BasePage(object):
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
-        except (NoSuchElementException):
+        except NoSuchElementException:
             return False
         return True
 
